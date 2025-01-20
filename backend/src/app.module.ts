4923cee8ed6acb21659d3eca589513e8349066c4
Chris Chan <chrisstudio1016@gmail.com>
2025-01-21 01:57:53 +0800

    feat: init user api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongoService } from './database/mongodb';
 import ServerConfig from './config/server.config';
 import { RedisService } from './database/redis';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [ServerConfig],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, MongoService, RedisService],
