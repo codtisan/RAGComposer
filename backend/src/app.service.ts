66dd7160b77dd2db01d1af032cd7bde080ff3258
Chris Chan <chrisstudio1016@gmail.com>
2025-01-21 01:49:55 +0800

    delete unused code and edit github action

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { MongoService } from './database/mongodb';
 import { HealthResponseDto } from './dto/health.dto';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly mongoService: MongoService) {}
+  constructor() {}
   async checkHealth(): Promise<HealthResponseDto> {
     return {
       statusCode: 200,
