3781087aea9031b0e645e968b7f976f8136ee079
Chris Chan <chrisstudio1016@gmail.com>
2025-01-21 01:39:03 +0800

    fix: jest test cases

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -16,7 +16,11 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHealth()).resolves.toEqual({
+        statusCode: 200,
+        message: 'OK',
+        timestamp: expect.any(Date),
+      });
     });
   });
 });
